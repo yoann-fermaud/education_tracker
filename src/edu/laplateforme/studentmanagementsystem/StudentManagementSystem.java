9fe5f550d1937c11dd513aa0de9bde309d9f182b
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-06-28 16:36:15 +0200

    Initial commit

---
+++ src/edu/laplateforme/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,7 @@
+package edu.laplateforme.studentmanagementsystem;
+
+public class StudentManagementSystem {
+    public static void main(String[] args) {
+        System.out.println("class: StudentManagementSystem");
+    }
+}
