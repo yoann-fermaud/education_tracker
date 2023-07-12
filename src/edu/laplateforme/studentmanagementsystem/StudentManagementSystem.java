3e70d9b73da44620011ea13f6809ae23f89c4fe3
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-07-12 17:06:08 +0200

    [ADD] main menu

--- src/edu/laplateforme/studentmanagementsystem/StudentManagementSystem.java
+++ src/edu/laplateforme/studentmanagementsystem/StudentManagementSystem.java
@@ -1,7 +1,23 @@
 package edu.laplateforme.studentmanagementsystem;
 
+import edu.laplateforme.studentmanagementsystem.common.OptionsHandler;
+import edu.laplateforme.studentmanagementsystem.common.UserInputHandler;
+
 public class StudentManagementSystem {
     public static void main(String[] args) {
-        System.out.println("class: StudentManagementSystem");
+        System.out.println("""
+
+                Student Management System
+                -------------------------
+                0 - Add a new student
+                1 - Modify a student
+                2 - Delete a student
+                3 - Find a student
+                4 - Show all students
+                """);
+
+        System.out.print("Choose an option: ");
+        short userInput = UserInputHandler.getUserInput();
+        OptionsHandler.processOption(userInput);
     }
 }
